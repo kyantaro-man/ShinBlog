d1193b7a9562525b68662d4a55ed7956ead5618d
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-08-06 17:17:12 +0900

    C of CRUD

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,22 @@
 class BlogsController < ApplicationController
 
   def index
+    @blogs = Blog.all
+    @blog = Blog.new
   end
-  
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: 'ブログを投稿しました'
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :text)
+  end
 end
